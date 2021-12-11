f2e0884f370132bd1b46f9543774a09cd41464df
arnu515 <arnu5152@gmail.com>
2021-12-11 15:43:08 +0530

    add ui elements

--- src/routes/chat/userId.tsx
+++ src/routes/chat/userId.tsx
@@ -6,8 +6,10 @@ import { encodeBase64 } from "tweetnacl-util";
 import Loading from "../../lib/components/Loading";
 import { chat } from "../../lib/db";
 import { genKeys } from "../../lib/e2e";
+import { friends as friendsStore } from "../../lib/stores/friends";
 import { user as userStore } from "../../lib/stores/user";
 import supabase from "../../lib/supabase";
+import c from "classnames";
 
 const ChatUserId: React.FC = () => {
   const [loading, setLoading] = React.useState(true);
@@ -16,6 +18,12 @@ const ChatUserId: React.FC = () => {
   const key = useLiveQuery(async () =>
     chat.keys.where("user_id").equals(params.userId!).first()
   );
+  const friends = useStore(friendsStore);
+  const [friend, setFriend] = React.useState<typeof friends[0] | null>();
+  const [friendProfile, setFriendProfile] = React.useState<
+    typeof friends[0]["from_profile"] | null
+  >();
+  const [message, setMessage] = React.useState("");
 
   React.useEffect(() => {
     // check if there are keys in localstorage, if not, generate new keys
@@ -28,6 +36,18 @@ const ChatUserId: React.FC = () => {
       localStorage.setItem("secretKey", encodeBase64(keys.secretKey));
     }
   });
+
+  React.useEffect(() => {
+    if (friends?.length) {
+      const f = friends.find(
+        (f) => f.from_id === params.userId || f.to_id === params.userId
+      );
+      setFriend(f);
+      setFriendProfile(
+        f && (f.from_id === params.userId ? f.from_profile : f.to_profile)
+      );
+    }
+  }, [friends, params]);
 
   React.useEffect(() => {
     if (!user) return;
@@ -48,10 +68,84 @@ const ChatUserId: React.FC = () => {
     } else setLoading(false);
   }, [key, params, user]);
 
-  if (loading) return <Loading />;
+  function sendMessage(e: React.FormEvent) {
+    e.preventDefault();
+    if (!message.trim()) return;
+
+    setMessage("");
+  }
+
+  if (loading || !friend || !friendProfile) return <Loading />;
 
   return (
-    <div className="shadow-lg bg-white border border-transparent rounded-lg h-full w-full flex gap-4 flex-col p-4"></div>
+    <div className="shadow-lg bg-white border border-transparent rounded-lg h-full w-full flex flex-col justify-between">
+      <div
+        className="border-b border-gray-300 p-4 flex items-center gap-4"
+        style={{
+          borderTopLeftRadius: "0.5rem",
+          borderTopRightRadius: "0.5rem",
+        }}
+      >
+        <img
+          src={friendProfile.avatar_url}
+          alt={`Avatar of ${friendProfile.name}`}
+          className="rounded-full border border-transparent shadow w-8"
+        />
+        <span className="font-bold text-xl">{friendProfile.name}</span>
+        <div className="flex items-center gap-2 ml-auto">
+          <button
+            aria-label="Remove friend"
+            title="Remove friend"
+            className="p-2 rounded-full cursor-pointer hover:bg-red-100 text-red-500 transition-colors duration-200"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <form onSubmit={sendMessage} className="flex gap-1 items-center">
+        <input
+          type="text"
+          className="border border-gray-500 outline-none rounded m-2 px-4 py-2 text-lg w-full"
+          placeholder="Enter a message and press enter"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          aria-label="Message"
+        />
+        <button
+          aria-label="Send message"
+          title="Send message"
+          className={c(
+            "p-2 rounded-full transition-colors duration-200 mr-2",
+            message.trim()
+              ? "cursor-pointer bg-blue-500 hover:bg-blue-600 text-white"
+              : "text-blue-500"
+          )}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-7 w-7"
+            style={{ transform: "rotate(90deg)" }}
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+          </svg>
+        </button>
+      </form>
+    </div>
   );
 };
 
